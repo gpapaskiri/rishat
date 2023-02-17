0b22ff851c11d983bea939cdcc23ced5ef5ec2f9
George Papaskiri <george.papaskiri@a-mobile.biz>
2023-02-17 23:23:33 +0300

    Modified settings.

--- rishat/settings.py
+++ rishat/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-6@im)ltr)fu$^ugjp#60p-e6$wh=99=*wwf)hu^x-^i_%gd3go
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
